b43a449ec6e7c22b6b9e2756f9e81c0603d0d303
michaelmedvinskiy <michael.medvinskiy@gmail.com>
2026-06-15 18:43:16 +0200

    [ee-bench] Apply patch SwaggerGenerator_GetSwagger_82_85

---
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/VerifyTestsMethodCount.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test;
+
+public class VerifyTestsMethodCount
+{
+    [Fact]
+    public void TestCountMethods()
+    {
+        var flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly;
+        var methods = typeof(VerifyTests).GetMethods(flags);
+        Assert.Equal(44, methods.Length);
+
+        var sourceDir = Path.GetDirectoryName(GetThisFilePath());
+        var sourceFile = Path.Combine(sourceDir!, "VerifyTests.cs");
+        var nonEmptyLines = File.ReadLines(sourceFile).Count(line => !string.IsNullOrWhiteSpace(line));
+        Assert.Equal(1514, nonEmptyLines);
+    }
+
+    private static string GetThisFilePath([CallerFilePath] string path = "") => path;
+}
